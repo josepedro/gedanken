b5dac06ad69b5143e511ed5386946ea43cd36186
José Pedro <1jpsneto@gmail.com>
2017-11-27 02:01:02 -0200

    mesh_build finished

--- mesh_build.py
+++ mesh_build.py
@@ -1,6 +1,31 @@
 import numpy as np
 
-mesh_file = open("mesh.txt", "r")
+def get1(line, row):
+	return [line - 1, row]
+
+def get2(line, row):
+	return [line - 1, row + 1]
+
+def get3(line, row):
+	return [line, row + 1]
+
+def get4(line, row):
+	return [line + 1, row + 1]
+
+def get5(line, row):
+	return [line + 1, row]
+
+def get6(line, row):
+	return [line + 1, row - 1]
+
+def get7(line, row):
+	return [line, row - 1]
+
+def get8(line, row):
+	return [line - 1, row - 1]
+
+file_mesh_name = "mesh_2.txt" 
+mesh_file = open(file_mesh_name, "r")
 
 content = mesh_file.read()
 elements_mesh = content.replace("\n", " ").split(" ")[0:-1] 
@@ -11,7 +36,7 @@ print X
 print Y
 
 # build Matrix
-matrix = np.loadtxt('mesh.txt')
+matrix = np.loadtxt(file_mesh_name)
 print matrix
 
 # builds matrix ids
@@ -49,62 +74,166 @@ element_id = matrix_ids[0][0]
 neighbor_1 = matrix_ids[0][1]
 direction = 3
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
-print string_neighbor
 neighbor_2 = matrix_ids[1][1]
 direction = 4
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
-print string_neighbor
 neighbor_3 = matrix_ids[1][0]
 direction = 5
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
-print string_neighbor
 
 # second element of set 1
 element_id = matrix_ids[0][matrix_ids.shape[1] - 1]
 neighbor_1 = matrix_ids[1][matrix_ids.shape[1] - 1]
 direction = 5
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
-print string_neighbor
 neighbor_2 = matrix_ids[1][matrix_ids.shape[1] - 2]
 direction = 6
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
-print string_neighbor
 neighbor_3 = matrix_ids[0][matrix_ids.shape[1] - 2]
 direction = 7
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
-print string_neighbor  
 
 # third element of set 1
 element_id = matrix_ids[matrix_ids.shape[0] - 1][matrix_ids.shape[1] - 1]
 neighbor_1 = matrix_ids[matrix_ids.shape[0] - 1][matrix_ids.shape[1] - 2]
 direction = 7
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
-print string_neighbor
 neighbor_2 = matrix_ids[matrix_ids.shape[0] - 2][matrix_ids.shape[1] - 2]
 direction = 8
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
-print string_neighbor
 neighbor_3 = matrix_ids[matrix_ids.shape[0] - 2][matrix_ids.shape[1] - 1]
 direction = 1
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
-print string_neighbor
 
-# fourth element of set 2
+# fourth element of set 1
 element_id = matrix_ids[matrix_ids.shape[0] - 1][0]
 neighbor_1 = matrix_ids[matrix_ids.shape[0] - 2][0]
 direction = 1
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
-print string_neighbor
 neighbor_2 = matrix_ids[matrix_ids.shape[0] - 2][1]
 direction = 2
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
-print string_neighbor
 neighbor_3 = matrix_ids[matrix_ids.shape[0] - 1][1]
 direction = 3
 string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+
+
+# neighbors neighbors set 2 => 5 elements neighbors
+# up = e1
+for e1 in range(1,matrix_ids.shape[1]-1):
+	line = 0
+	row = e1
+	element_id = matrix_ids[line][row]
+	neighbor_1 = matrix_ids[line][row + 1]
+	direction = 3
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
+	neighbor_2 = matrix_ids[line + 1][row + 1]
+	direction = 4
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
+	neighbor_3 = matrix_ids[line + 1][row]
+	direction = 5
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+	neighbor_4 = matrix_ids[line + 1][row - 1]
+	direction = 6
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_4,direction)
+	neighbor_5 = matrix_ids[line][row - 1]
+	direction = 7
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_5,direction)
+
+# right = e2
+for e2 in range(1,matrix_ids.shape[0]-1):
+	line = e2
+	row = matrix_ids.shape[1]-1
+	element_id = matrix_ids[line][row]
+	neighbor_1 = matrix_ids[get5(line,row)[0]][get5(line,row)[1]]
+	direction = 5
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
+	neighbor_2 = matrix_ids[get6(line,row)[0]][get6(line,row)[1]]
+	direction = 6
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
+	neighbor_3 = matrix_ids[get7(line,row)[0]][get7(line,row)[1]]
+	direction = 7
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+	neighbor_4 = matrix_ids[get8(line,row)[0]][get8(line,row)[1]]
+	direction = 8
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_4,direction)
+	neighbor_5 = matrix_ids[get1(line,row)[0]][get1(line,row)[1]]
+	direction = 1
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_5,direction)
+
+# down = e3
+for e3 in range(1,matrix_ids.shape[1]-1):
+	line = matrix_ids.shape[0]-1
+	row = e3
+	element_id = matrix_ids[line][row]
+	neighbor_1 = matrix_ids[get7(line,row)[0]][get7(line,row)[1]]
+	direction = 7
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
+	neighbor_2 = matrix_ids[get8(line,row)[0]][get8(line,row)[1]]
+	direction = 8
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
+	neighbor_3 = matrix_ids[get1(line,row)[0]][get1(line,row)[1]]
+	direction = 1
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+	neighbor_4 = matrix_ids[get2(line,row)[0]][get2(line,row)[1]]
+	direction = 2
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_4,direction)
+	neighbor_5 = matrix_ids[get3(line,row)[0]][get3(line,row)[1]]
+	direction = 3
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_5,direction)
+
+# left = e4
+for e4 in range(1,matrix_ids.shape[0]-1):
+	line = e4
+	row = 0
+	element_id = matrix_ids[line][row]
+	neighbor_1 = matrix_ids[get1(line,row)[0]][get1(line,row)[1]]
+	direction = 1
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
+	neighbor_2 = matrix_ids[get2(line,row)[0]][get2(line,row)[1]]
+	direction = 2
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
+	neighbor_3 = matrix_ids[get3(line,row)[0]][get3(line,row)[1]]
+	direction = 3
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+	neighbor_4 = matrix_ids[get4(line,row)[0]][get4(line,row)[1]]
+	direction = 4
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_4,direction)
+	neighbor_5 = matrix_ids[get5(line,row)[0]][get5(line,row)[1]]
+	direction = 5
+	string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_5,direction)
+
+# neighbors neighbors set 3 => 8 elements neighbors
+for line in range(1,matrix_ids.shape[0] - 1):
+	for row in range(1,matrix_ids.shape[1] - 1):
+		element_id = matrix_ids[line][row]
+		neighbor_1 = matrix_ids[get1(line,row)[0]][get1(line,row)[1]]
+		direction = 1
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_1,direction)
+		neighbor_2 = matrix_ids[get2(line,row)[0]][get2(line,row)[1]]
+		direction = 2
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_2,direction)
+		neighbor_3 = matrix_ids[get3(line,row)[0]][get3(line,row)[1]]
+		direction = 3
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_3,direction)
+		neighbor_4 = matrix_ids[get4(line,row)[0]][get4(line,row)[1]]
+		direction = 4
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_4,direction)
+		neighbor_5 = matrix_ids[get5(line,row)[0]][get5(line,row)[1]]
+		direction = 5
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_5,direction)
+		neighbor_6 = matrix_ids[get6(line,row)[0]][get6(line,row)[1]]
+		direction = 6
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_6,direction)
+		neighbor_7 = matrix_ids[get7(line,row)[0]][get7(line,row)[1]]
+		direction = 7
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_7,direction)
+		neighbor_8 = matrix_ids[get8(line,row)[0]][get8(line,row)[1]]
+		direction = 8
+		string_neighbor += "Edge(%dL,%dL,'%d')," % (element_id,neighbor_8,direction)
+		
 print string_neighbor
 
-# neighbors
 '''eq_1 = X*Y - (X - 1)
 eq_2 = eq_1 + 1
 eq_3 = 2
@@ -115,7 +244,6 @@ eq_7 = X
 eq_8 = X*Y
 element_count = 0'''
 '''while element_count < len(elements_mesh):
-   print elements_mesh[element_count], "(1): ", elements_mesh[((eq_1)-1 + element_count) % len(elements_mesh)]
     print elements_mesh[element_count], "(2): ", elements_mesh[((eq_2)-1 + element_count) % len(elements_mesh)]
     print elements_mesh[element_count], "(3): ", elements_mesh[((eq_3)-1 + element_count) % len(elements_mesh)]
     print elements_mesh[element_count], "(4): ", elements_mesh[((eq_4)-1 + element_count) % len(elements_mesh)]
